e2cb2ed800b4bea3345f8527070c720a01f5f1fa
Arnold Sandoval <arnold@arnie.io>
2019-04-23 07:45:54 -0700

    Updates

---
+++ precache-manifest.cd2a721c42020ce50d2abe1e8b6be234.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "767911c0b51cf4bf0e07c412ec599038",
+    "url": "/slds-challenge/static/media/illustration-rainy.767911c0.svg"
+  },
+  {
+    "revision": "f4c16cbccbd3722046aff40805bb031a",
+    "url": "/slds-challenge/static/media/icon-delete.f4c16cbc.svg"
+  },
+  {
+    "revision": "ee72f609e6faa6c701cbb945dbc37740",
+    "url": "/slds-challenge/static/media/icon-cart.ee72f609.svg"
+  },
+  {
+    "revision": "dca45216dc087896e4b5",
+    "url": "/slds-challenge/static/js/runtime~main.cec2ffa2.js"
+  },
+  {
+    "revision": "40cfae2208da42d578ef",
+    "url": "/slds-challenge/static/js/main.2ecae38e.chunk.js"
+  },
+  {
+    "revision": "a03702d8d199df1dcbfc",
+    "url": "/slds-challenge/static/js/2.84d6790b.chunk.js"
+  },
+  {
+    "revision": "40cfae2208da42d578ef",
+    "url": "/slds-challenge/static/css/main.f7f410ad.chunk.css"
+  },
+  {
+    "revision": "bbcae7335d44731ef4371512232c2f29",
+    "url": "/slds-challenge/index.html"
+  }
+];
